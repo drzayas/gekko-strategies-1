9c97c318f5a12a5420087703064a6b890e0dd45a
Christof Schraepen <christof@emilioit.com>
2019-01-07 11:31:25 +0100

    Initial setup of the indicator

---
+++ indicators/TDM_Seq.js
@@ -0,0 +1,39 @@
+/*
+ * Tom DeMark's Sequential indicator
+ *
+ * Returns an object with properties:
+ *
+ * numberOfCountdowns: Number of countdowns so far (integer)
+ * countdownType: current countdown type (buy/sell)
+ * perfectSetup: countdown has a perfect setup? (boolean)
+ *
+ */
+
+let Indicator = function() {
+    this.historyData = [];
+    this.historyMax = 20;
+    this.state = 'none';
+    this.perfectSetup = null;
+    this.resetState();
+    this.result = {numberOfCountdowns: 0, countdownType: null, perfectSetup: false};
+    console.log('TDM_Seq: indicator initialised');
+};
+
+Indicator.prototype.update = function(candle) {
+    this.addToHistoryData(candle);
+};
+
+Indicator.prototype.addToHistoryData = function(candle) {
+    this.historyData.unshift(candle);
+    if (this.historyData.length > this.historyMax) {
+        this.historyData.pop();
+    }
+    console.log('TDM_Seq: Added candle: ', JSON.stringify(candle));
+};
+
+Indicator.prototype.resetState = function() {
+    this.state = 'none';
+    this.perfectSetup = false;
+};
+
+module.exports = Indicator;
