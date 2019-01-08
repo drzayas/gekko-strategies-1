2f3e7b04ac9eea11d5190e84319104b7fb60cd95
Christof Schraepen <christof@emilioit.com>
2019-01-08 10:49:04 +0100

    Detect Price flips required to start the Setup procedure

--- indicators/TDM_Seq.js
+++ indicators/TDM_Seq.js
@@ -15,11 +15,16 @@ let Indicator = function() {
     this.state = 'none';
     this.perfectSetup = false;
     this.result = {numberOfCountdowns: 0, countdownType: null, perfectSetup: false};
+    this.bearishPriceFlip = false;
+    this.bullishPriceFlip = false;
     console.log('TDM_Seq: indicator initialised');
 };
 
 Indicator.prototype.update = function(candle) {
     this.addToHistoryData(candle);
+    if (this.historyData.length > 5) {
+        this.updateCounters();
+    }
 };
 
 Indicator.prototype.addToHistoryData = function(candle) {
@@ -27,7 +32,29 @@ Indicator.prototype.addToHistoryData = function(candle) {
     if (this.historyData.length > this.historyMax) {
         this.historyData.pop();
     }
-    console.log('TDM_Seq: Added candle: ', JSON.stringify(candle));
+    //console.log('TDM_Seq: Added candle: ', JSON.stringify(candle));
+};
+
+Indicator.prototype.updateCounters = function() {
+    //First detect price flips
+    //Bearish Price Flip - occurs when the market records a close greater than the close four bars earlier, immediately followed by a close less than the close four bars earlier.
+
+    if(
+        (this.historyData[1].close > this.historyData[5].close) &&
+        (this.historyData[0].close < this.historyData[4].close)
+    ) {
+        console.log("TDM_Seq Indicator: Bearish price flip detected at " + JSON.stringify(this.historyData[0]));
+        this.bearishPriceFlip = true;
+    }
+    //Bullish Price Flip - occurs when the market records a close less than the close four bars before, immediately followed by a close greater than the close four bars earlier.
+    if(
+        (this.historyData[1].close < this.historyData[5].close) &&
+        (this.historyData[0].close > this.historyData[4].close)
+    ) {
+        console.log("TDM_Seq Indicator: Bullish price flip detected at " + JSON.stringify(this.historyData[0]));
+        this.bullishPriceFlip = true;
+    }
+
 };
 
 Indicator.prototype.resetState = function() {
